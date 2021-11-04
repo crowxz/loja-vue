a49bccef47435d42e5b05e9cf0a43a04ee8b6d6f
lucas leite <lucasleite.bv@gmail.com>
2021-11-04 08:21:27 -0400

    Fist commit

---
+++ src/data/products.js
@@ -0,0 +1,73 @@
+export default [{
+        id: 1,
+        name: "EC3 Cyan ",
+        piece: 1,
+        price: 1250,
+        image: require("../assets/images/cadeira1.jpg"),
+        color: "blue",
+        description: "Cadeira Gamer EC3 Cyan THUNDERX3 AZUL",
+        category: "Gamer",
+        rating: 4,
+        sizes: [
+            { text: "70kg", value: "70kg" },
+            { text: "90kg", value: "90kg" },
+            { text: "120kg", value: "120kg", disabled: false },
+            { text: "150kg", value: "150kg", disabled: false },
+
+
+        ],
+    },
+    {
+        id: 2,
+        name: "EC3 Cyan",
+        piece: 1,
+        price: 1400,
+        image: require("../assets/images/cadeira3.jpg"),
+        color: "red",
+        description: "Cadeira Gamer EC3 Cyan THUNDERX3",
+        category: "Gamer",
+        rating: 4,
+        sizes: [
+            { text: "70kg", value: "70kg" },
+            { text: "90kg", value: "90kg" },
+            { text: "120kg", value: "120kg", disabled: false },
+            { text: "150kg", value: "150kg", disabled: false },
+
+
+        ],
+    },
+    {
+        id: 3,
+        name: "EC3 Cyan",
+        piece: 1,
+        price: 1200,
+        image: require("../assets/images/cadeira2.jpg"),
+        color: "green",
+        description: "Cadeira Gamer EC3 Cyan THUNDERX3",
+        category: "Gamer",
+        rating: 3,
+        sizes: [
+            { text: "70kg", value: "70kg" },
+            { text: "90kg", value: "90kg" },
+            { text: "120kg", value: "120kg", disabled: false },
+            { text: "150kg", value: "150kg", disabled: false },
+        ],
+    },
+    {
+        id: 4,
+        name: "EC3 Cyan",
+        piece: 1,
+        price: 1570,
+        image: require("../assets/images/cadeira4.jpg"),
+        color: "blue/black",
+        description: "Cadeira Gamer EC3 Cyan THUNDERX3",
+        category: "Gamer",
+        rating: 5,
+        sizes: [
+            { text: "70kg", value: "70kg" },
+            { text: "90kg", value: "90kg" },
+            { text: "120kg", value: "120kg", disabled: false },
+            { text: "150kg", value: "150kg", disabled: false },
+        ],
+    },
+];
